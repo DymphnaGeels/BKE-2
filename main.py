90d75d11f325e131b583efa461c51ba7bc89c3f6
Dymphna Geels <57255075+DymphnaGeels@users.noreply.github.com>
2021-12-17 10:51:29 +0000

    2.3 Inzicht tijdens het trainen.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from _ml import MLAgent, train, save, load, train_and_plot, RandomAgent, validate, plot_validation
 from _core import is_winner, opponent, start 
 
+start(MLAgent)
+
 class MyAgent(MLAgent):
   def evaluate(self, board):
       if is_winner(board, self.symbol):
